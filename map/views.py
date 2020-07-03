5cac222fc455204f68dc2fda8e523c8858dae87d
Ariel Young <ayyoung9@gmail.com>
2020-07-02 19:57:25 -0500

    Grab apikey from envir and pass to view

--- map/views.py
+++ map/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+import os
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+    GOOGLE_ENDPOINT = f'https://maps.googleapis.com/maps/api/js?key={GOOGLE_API_KEY}&callback=initMap'
+    return render(request, 'map/index.html', {'GOOGLE_ENDPOINT': GOOGLE_ENDPOINT})
