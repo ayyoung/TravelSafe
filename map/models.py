989360b0ad7c917254155e67a8d396c5dfe60fe3
Ariel Young <ayyoung9@gmail.com>
2020-07-02 19:07:43 -0500

    Add model of CrimeReport

--- map/models.py
+++ map/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    date = models.DateTimeField('date')
+    longitude = models.DecimalField(max_digits=7, decimal_places=5)
+    lattitude = models.DecimalField(max_digits=7, decimal_places=5)
+    crime_type = models.TextField(max_length=32) 
